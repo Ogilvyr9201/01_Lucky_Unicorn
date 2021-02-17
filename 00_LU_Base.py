36a69c412aa73028f7f9dfcdea3aa2cd8985510d
Ogilvyr9201 <78511761+Ogilvyr9201@users.noreply.github.com>
2021-02-18 12:10:20 +1300

    Checkpoint 2
    made function and added to base code

--- 00_LU_Base.py
+++ 00_LU_Base.py
@@ -16,6 +16,7 @@ def yes_no(question):
             print("<error> Please say yes or no")
             print()
 
+
 def instructions():
     print("**** How to Play ****")
     print()
@@ -23,11 +24,40 @@ def instructions():
     print()
     return""
 
+
+def num_check(question, low, high):
+    error = "Please enter a whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask question
+            response = int(input(question))
+
+            # if response is too high
+            if low < response <= high:
+               return response
+
+            # output with error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 # Main routines
+
+# Ask if the user has played before
 played_before = yes_no("Have you played this game before? ")
 
 if played_before == "no":
     instructions()
 
-print("Program continues")
+print()
+
+# Ask how much the user how much they wamt to play with
+how_much = num_check("How much would you like to play with? ", 0, 10)
+
+print("You have asked to play with {}.".format(how_much))
 
--- 04_how_much_v2.py
+++ 04_how_much_v2.py
@@ -1,23 +1,25 @@
 # Functions
+def num_check(question, low, high):
+    error = "Please enter a whole number between 1 and 10\n"
 
+    valid = False
+    while not valid:
+        try:
+            # ask question
+            response = int(input(question))
 
-# main routine
+            # if response is too high
+            if low < response <= high:
+               return response
 
-error = "Please enter a whole number between 1 and 10\n"
+            # output with error
+            else:
+                print(error)
 
-valid = False
-while not valid:
-    try:
-        # ask question
-        response = int(input("How much would you like to play with? "))
-
-        # if response is too high
-        if 0 < response <= 10:
-            print("You have asked to play with {}.".format(response))
-
-        # output with error
-        else:
+        except ValueError:
             print(error)
 
-    except ValueError:
-        print(error)
+# main routine
+how_much = num_check("How much would you like to play with? ", 0, 10)
+
+print("You have asked to play with {}.".format(how_much))

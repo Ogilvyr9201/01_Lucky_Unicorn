9b422bab0481091e36d4b61831179bf5b617c44f
Ogilvyr9201 <78511761+Ogilvyr9201@users.noreply.github.com>
2021-02-25 12:20:33 +1300

    Game base
    Game base complete now to decoration

--- 00_LU_Base.py
+++ 00_LU_Base.py
@@ -1,3 +1,5 @@
+import random
+
 # Functions
 def yes_no(question):
     valid = False
@@ -20,8 +22,8 @@ def yes_no(question):
 def instructions():
     print("**** How to Play ****")
     print()
-    print("The rules go here")
-    print()
+    print("In this game the aim is to get as much money as you can! ")
+    print("")
     return""
 
 
@@ -59,5 +61,57 @@ print()
 # Ask how much the user how much they wamt to play with
 how_much = num_check("How much would you like to play with? ", 0, 10)
 
-print("You have asked to play with {}.".format(how_much))
+balance = how_much
+
+rounds_played = 0
+
+play_again = input("Press <enter> to Play ").lower()
+while play_again == "":
+
+    # increase number of rounds played
+    rounds_played += 1
+
+    # print round number
+    print()
+    print("***Round #{}***".format(rounds_played))
+    chosen_num = random.randint(1, 100)
+
+    # adjust balance
+    # if random picks 1-5
+    # user gets a unicorn: +$4.00
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+
+    # if random picks 6-36
+    # user gets a donkey: -$1.00
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+    # if random picks -5
+    # user gets a horse/donkey: -$0.50
+    else:
+        # if num is even token is horse
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+
+        # other wise its zebra
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    # Output
+    print("You got {}. Your balance is ${:.2f}".format(chosen, balance))
+
+
+    if balance < 1:
+        play_again = "xxx"
+        print()
+        print("Sorry you have run out of money :(")
+    else:
+        print()
+        play_again = input(" Press <enter> to play agan "
+                           "or 'xxx' to quit ")
+print()
+print("Final Balance: $", balance)
 
--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,111 +1,12 @@
-# Higher lower game V1, 16/09/20
-# V1 Basic Program
-# V2 program loops
-# V3 best guess is added
-# V4 Adds a win and lose
-# V5 Tidy up code
+greeting = "hello world"
+sides = "$" * 3
 
-import random
+hello = "{} {} {}".format(sides , greeting, sides)
 
+top_bottom = "-"  * len(greeting) * 2
 
-def num_check(num_type, question, num):
-    valid = False
-    while not valid:
-        try:
-            response = num_type(input(question))
-            if num < response:
-                valid = True
-                return response
-            else:
-                print("Please enter a number more then {}".format(num))
-                print()
-        except ValueError:
-            print("Please enter a number more then {}".format(num))
-            print()
+print(hello)
 
-
-# Title and instructions
-print("Higher / Lower Guessing game")
-print("All you have to is guess the randomly generated number")
-start = input("Press <enter> to start! ")
-
-if start == "":
-    # Ask user for integers
-    print()
-    print()
-
-    low_integer = num_check(int, "What is your Low number? ", 0)
-
-    high_integer = num_check(int, "What is your High Number? ", low_integer)
-
-    num_guess = num_check(int, "How many Guesses do you get? ", 0)
-
-    rounds = num_check(int, "How many Rounds do you want to play? ", 0)
-    print()
-
-    # Start
-    best_guess = "_"
-    num_rounds = 0
-    while num_rounds < rounds:
-
-        # pick a Random number
-        secret = random.randrange(low_integer, high_integer)
-        print()
-
-        num_guesses = 0
-        guess = -1
-        if num_guesses == num_guess:
-            print("You did not get the number in the amont of guesses given :(")
-            print()
-        else:
-            while num_guesses < num_guess and guess != secret:
-                valid_guess = False
-                while not valid_guess:
-
-                    try:
-                        guess = int(input("Guess a number between {} and {}:   ".format(low_integer, high_integer)))
-                        print()
-
-                        if guess < low_integer or guess > high_integer:
-                            print("Your Guess is not valid plesae enter something between {} and {}".format(low_integer,
-                                                                                                            high_integer))
-                            print()
-
-                        else:
-                            num_guesses += 1
-                            valid_guess = True
-
-                    except ValueError:
-                        print("Your Guess is not valid plesae enter something between {} and {}".format(low_integer,
-                                                                                                        high_integer))
-                        print()
-
-                # Compare guesses
-                if num_guesses == num_guess:
-                    print("You lost, you didnt get the number within the specified amount of guesses.")
-                    num_rounds += 1
-                elif guess < secret:
-                    print("Its higher")
-                    print()
-                elif guess > secret:
-                    print("Its Lower")
-                    print()
-                else:
-                    win = ""
-                    print("Correct")
-                    print()
-                    num_rounds += 1
-                    if win == "":
-                        print("Congratulations you geussed it in {} trys".format(num_guesses))
-
-                        # Update Best Guess
-                        if num_guesses < best_guess:
-                            best_guess = num_guesses
-
-# Finish
-print()
-print("Out of your {} rounds your Best Score was {} guesses".format(rounds, best_guess))
-
-# Jesus
-
-jesus = "jesus is the meaning of life"
+print(top_bottom)
+print("{}  {}  {}".format(sides , greeting, sides))
+print(top_bottom)
